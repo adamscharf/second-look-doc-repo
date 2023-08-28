f4aad89e38cf791b149798abeb641284f15684ef
Adam Scharf <scharfat@gmail.com>
2023-08-28 14:40:57 -0400

    Add initial app code

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# second-look-doc-repo
+# Second Look: A Simple Document Repository
+
 Document repository application built using SAM CLI
+
+## Description
+
+API Gateway -> AWS Lambda -> DynamoDB -> Client -> S3
+
+When an HTTP POST request is made to the Amazon API Gateway endpoint, the AWS Lambda function is invoked which generates a pre-signed S3 POST URL and inserts a placeholder item into the Amazon DynamoDB table. It then returns the pre-signed URL for the client to use to upload a file to S3.
+
+Inspiration and code snippets were taken from several Serverless Land Patterns. Learn more about the used patterns and more here:
+
+- [apigw-lambda-dynamodb](https://serverlessland.com/patterns/apigw-lambda-dynamodb)
+
+> **Important**: This application uses various AWS services and there are costs associated with these services after the Free Tier usage - please see the [AWS Pricing page](https://aws.amazon.com/pricing/) for details. You are responsible for any AWS costs incurred. No warranty is implied in this example.
+
+## Requirements
+
+* [Create an AWS account](https://portal.aws.amazon.com/gp/aws/developer/registration/index.html) if you do not already have one and log in. The IAM user that you use must have sufficient permissions to make necessary AWS service calls and manage AWS resources.
+* [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html) installed and configured
+* [Git Installed](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+* [AWS Serverless Application Model](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html) (AWS SAM) installed
+
+## Deployment Instructions
+
+1. Create a new directory, navigate to that directory in a terminal and clone the GitHub repository:
+    ```bash
+    git clone https://github.com/adamscharf/second-look-doc-repo
+    ```
+1. Change directory to the pattern directory:
+    ```bash
+    cd second-look-doc-repo/
+    ```
+1. From the command line, use AWS SAM to deploy the AWS resources for the pattern as specified in the template.yml file:
+    ```bash
+    sam deploy --guided
+    ```
+1. During the prompts:
+    * Enter a stack name
+    * Enter the desired AWS Region
+    * Allow SAM CLI to create IAM roles with the required permissions.
+
+    Once you have run `sam deploy --guided` mode once and saved arguments to a configuration file (samconfig.toml), you can use `sam deploy` in future to use these defaults.
+
+1. Note the outputs from the SAM deployment process. These contain the resource names and/or ARNs which are used for testing.
+
+## How it works
+
+When an HTTP POST request is sent to the Amazon API Gateway endpoint, the AWS Lambda function is invoked and creates a presigned URL which is returned to the client. It also inserts a placeholder item into the Amazon DynamoDB table with a contentUrl link to the future upload location. The client is then able to use the pre-signed URL to upload their file to the S3 bucket.
+
+## Cleanup
+
+1. Delete the stack
+    ```bash
+    aws cloudformation delete-stack --stack-name STACK_NAME
+    ```
+1. Confirm the stack has been deleted
+    ```bash
+    aws cloudformation list-stacks --query "StackSummaries[?contains(StackName,'STACK_NAME')].StackStatus"
+    ```
---
+++ src/generate-url/main.py
@@ -0,0 +1,118 @@
+import uuid
+import os
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver
+from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools import Tracer
+from aws_lambda_powertools import Logger
+from aws_lambda_powertools import Metrics
+from aws_lambda_powertools.metrics import MetricUnit
+
+app = APIGatewayRestResolver()
+logger = Logger()
+tracer = Tracer()
+logger = Logger()
+metrics = Metrics(namespace="PowertoolsSample")
+
+dynamodb_client = boto3.client('dynamodb')
+s3_client = boto3.client('s3')
+
+s3_bucket_name = os.environ.get('QuarantineBucketName')
+dynamodb_table_name = os.environ.get('DynamoDBTableName')
+
+
+def create_presigned_post(bucket_name: str,
+                          object_name: str,
+                          fields: dict = None,
+                          conditions: list = None,
+                          expiration: int = 600):
+    """Generate a presigned URL S3 POST request to upload a file
+
+    :param bucket_name: string
+    :param object_name: string
+    :param fields: Dictionary of prefilled form fields
+    :param conditions: List of conditions to include in the policy
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Dictionary with the following keys:
+        url: URL to post to
+        fields: Dictionary of form fields and values to submit with the POST
+    :return: None if error.
+    """
+
+    # Generate a presigned S3 POST URL
+    try:
+        response = s3_client.generate_presigned_post(bucket_name,
+                                                     object_name,
+                                                     Fields=fields,
+                                                     Conditions=conditions,
+                                                     ExpiresIn=expiration)
+    except ClientError as e:
+        logger.error(e)
+        return None
+
+    # The response contains the presigned URL and required fields
+    return response
+
+@app.post("/")
+@tracer.capture_method
+def create_url():
+    # adding custom metrics
+    # See: https://docs.powertools.aws.dev/lambda-python/latest/core/metrics/
+    metrics.add_metric(name="GeneratePresignedUrlInvocations", unit=MetricUnit.Count, value=1)
+
+    id: str = str(uuid.uuid4())
+
+    logger.info(f"Generated id: {id}")
+  
+    object_name: str = f'quarantine/{id}/{id}.bin'
+    s3_response = create_presigned_post(s3_bucket_name, object_name)
+
+    if s3_response is not None:
+      try:
+          logger.info(f'Creating an entry in the DynamoDB table: {dynamodb_table_name}')
+          dynamodb_response = dynamodb_client.put_item(TableName=dynamodb_table_name, Item={'id': {'S': id}, 'ContentUrl': {'S': f's3://{s3_bucket_name}/{object_name}'}, 'Status': {'S': 'Pending User Upload'}, 'Properties': {'M': {}}})
+      except ClientError as e:
+          logger.error(e)
+          dynamodb_response = None
+
+      if dynamodb_response is not None:
+        logger.info('HTTP 202 - Created presigned URL and returning it to the user.')
+      
+        return {
+        'statusCode': 202,
+        'body': {
+          'id': id,
+          'text': 'Presigned URL created. Please proceed with your upload.',
+          'presignedUrlDetails': {
+              'url': s3_response.get('url'),
+              'fields': s3_response.get('fields')
+          }
+        }
+      }
+      else:
+        logger.error('HTTP Status: 500 - Something went wrong while updating the DB.')
+        return {
+          'statusCode': 500,
+          'body': 'Something went wrong while updating the database. Please try again later.'
+      }
+    else:
+       logger.error('HTTP Status: 500 - Something went wrong while generating the presigned URL.')
+       return {
+        'statusCode': 500,
+        'body': 'Something went wrong while generating the presigned URL. Please try again later.'
+     }
+
+
+# Enrich logging with contextual information from Lambda
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
+# Adding tracer
+# See: https://docs.powertools.aws.dev/lambda-python/latest/core/tracer/
+@tracer.capture_lambda_handler
+# ensures metrics are flushed upon request completion/failure and capturing ColdStart metric
+@metrics.log_metrics(capture_cold_start_metric=True)
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+  return app.resolve(event, context)
